00bcf59339f2a2e363632a80fc16430c4bce2d8d
jestoniandales25 <andalesjestoni1998@gmail.com>
2026-03-12 13:49:36 +0800

    feat: added hololive data for the dashboard and details data to use

---
+++ lib/datas/hololive_data.dart
@@ -0,0 +1,81 @@
+import 'package:hololive_id_cards/models/hololive_model.dart';
+
+final List<HololiveModel> hololiveMembers = [
+  HololiveModel(
+    id: '1',
+    imageUrl:
+        'https://preview.redd.it/sora-chans-new-twitter-pfp-v0-8gd7b9y74tea1.jpg?auto=webp&s=31b9b66cc08ba41bccfb5a0b12011de156bc9c26',
+    name: 'Tokino Sora',
+    jpName: 'ときのそら',
+    debutDate: 'September 7th, 2017',
+    height: '160 cm',
+    birthday: 'May 15th',
+    fanBaseName: 'Sora-tomo',
+    generation: 0,
+    languageSpoken: 'Japanese',
+    youtubeChannel: 'https://www.youtube.com/channel/UCp6993wxpyDPHUpavwDFqgg',
+    twitter: 'https://x.com/tokino_sora',
+  ),
+
+  HololiveModel(
+    id: '2',
+    imageUrl: 'https://static.zerochan.net/Roboco.full.3356706.jpg',
+    name: 'Roboco',
+    jpName: 'ロボ子さん',
+    debutDate: 'March 4th, 2018',
+    height: '154 cm',
+    birthday: 'May 23rd',
+    fanBaseName: 'Roboser',
+    generation: 0,
+    languageSpoken: 'Japanese',
+    youtubeChannel: 'https://www.youtube.com/channel/UCDqI2jOz0weumE8s7paEk6g',
+    twitter: 'https://x.com/robocosan',
+  ),
+
+  HololiveModel(
+    id: '3',
+    imageUrl:
+        'https://i.pinimg.com/736x/55/ec/22/55ec227f1963b2f388c2d38ebf611a59.jpg',
+    name: 'Sakura Miko',
+    jpName: 'さくらみこ',
+    debutDate: 'December 25th, 2018',
+    height: '152 cm',
+    birthday: 'March 5th',
+    fanBaseName: '35P',
+    generation: 0,
+    languageSpoken: 'Japanese',
+    youtubeChannel: 'https://www.youtube.com/channel/UC-hM6YJuNYVAmUWxeIr9FeA',
+    twitter: 'https://x.com/sakuramiko35',
+  ),
+
+  HololiveModel(
+    id: '4',
+    imageUrl:
+        'https://w0.peakpx.com/wallpaper/274/399/HD-wallpaper-anime-virtual-youtuber-hoshimachi-suisei-hololive.jpg',
+    name: 'Hoshimachi Suisei',
+    jpName: '星街すいせい',
+    debutDate: 'March 22nd, 2018',
+    height: '160 cm',
+    birthday: 'March 22nd',
+    fanBaseName: 'Kometomo',
+    generation: 0,
+    languageSpoken: 'Japanese',
+    youtubeChannel: 'https://www.youtube.com/channel/UC5CwaMl1eIgY8h02uZw7u8A',
+    twitter: 'https://x.com/suisei_hosimati',
+  ),
+
+  HololiveModel(
+    id: '5',
+    imageUrl: 'https://images5.alphacoders.com/130/1304698.png',
+    name: 'Azki',
+    jpName: 'あずき',
+    debutDate: 'November 15th, 2018',
+    height: '158 cm',
+    birthday: 'July 1st',
+    fanBaseName: 'Kaitakusha',
+    generation: 0,
+    languageSpoken: 'Japanese',
+    youtubeChannel: 'https://www.youtube.com/channel/UC0TXe_LYZ4scaW2XMyi5_kw',
+    twitter: 'https://x.com/AZKi_VDiVA',
+  ),
+];
