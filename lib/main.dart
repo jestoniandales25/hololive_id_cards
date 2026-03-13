476bbdab1dbd3b29d8b3a13dcbb551ae92a97eaa
jestoniandales25 <andalesjestoni1998@gmail.com>
2026-03-13 17:01:46 +0800

    feat: refactor main app structure to use HololiveBloc and update routing

--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,35 @@
 import 'package:flutter/material.dart';
-import 'package:hololive_id_cards/pages/dashboard/hololive_page.dart';
-import 'package:hololive_id_cards/pages/favorites/favorites_page.dart';
-import 'package:hololive_id_cards/providers/hololive_provider.dart';
+import 'package:hololive_id_cards/blocs/hololive_bloc.dart';
+import 'package:hololive_id_cards/data/repositories/hololive_repository.dart';
+import 'package:hololive_id_cards/ui/screens/members_screen.dart';
+import 'package:hololive_id_cards/ui/screens/splash_screen.dart';
 import 'package:provider/provider.dart';
 
 void main() {
-  runApp(
-    ChangeNotifierProvider(
-      create: (context) => HololiveProvider(),
-      child: const HololiveMainApp(),
-    ),
-  );
+  runApp(const MyApp());
 }
 
-class HololiveMainApp extends StatelessWidget {
-  const HololiveMainApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Hololive ID Cards',
-      theme: ThemeData(primarySwatch: Colors.red),
-      initialRoute: '/',
-      routes: {
-        '/': (context) => const HololivePage(),
-        '/favorites': (context) => const FavoritesPage(),
-      },
+    return ChangeNotifierProvider(
+      // 👇 creates bloc AND calls loadMembers() immediately
+      create: (_) => HololiveBloc(HololiveRepository())..loadMembers(),
+      child: MaterialApp(
+        title: 'Hololive Members',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData.dark().copyWith(
+          scaffoldBackgroundColor: const Color(0xFF0D0D0D),
+          colorScheme: const ColorScheme.dark(primary: Color(0xFF00ADB5)),
+        ),
+        initialRoute: '/',
+        routes: {
+          '/': (context) => const SplashScreen(),
+          '/members': (context) => const MembersScreen(),
+        },
+      ),
     );
   }
 }
