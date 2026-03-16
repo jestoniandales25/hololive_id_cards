b87da16926c5a12f9bc8433bff57bbec3ffdf52f
jestoniandales25 <andalesjestoni1998@gmail.com>
2026-03-16 17:25:57 +0800

    feat: add member detail screen and refactor main app structure

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,18 @@
 import 'package:flutter/material.dart';
-import 'package:hololive_id_cards/blocs/hololive_bloc.dart';
-import 'package:hololive_id_cards/data/repositories/hololive_repository.dart';
-import 'package:hololive_id_cards/ui/screens/members_screen.dart';
-import 'package:hololive_id_cards/ui/screens/splash_screen.dart';
 import 'package:provider/provider.dart';
+import 'blocs/hololive_bloc.dart';
+import 'data/repositories/hololive_repository.dart';
+import 'ui/screens/members_screen.dart';
+import 'ui/screens/splash_screen.dart';
+import 'ui/screens/member_detail_screen.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    ChangeNotifierProvider(
+      create: (_) => HololiveBloc(HololiveRepository())..loadMembers(),
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -14,22 +20,21 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      // 👇 creates bloc AND calls loadMembers() immediately
-      create: (_) => HololiveBloc(HololiveRepository())..loadMembers(),
-      child: MaterialApp(
-        title: 'Hololive Members',
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData.dark().copyWith(
-          scaffoldBackgroundColor: const Color(0xFF0D0D0D),
-          colorScheme: const ColorScheme.dark(primary: Color(0xFF00ADB5)),
+    return MaterialApp(
+      title: 'Hololive Members',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: const Color(0xFF0D0D0D),
+        colorScheme: const ColorScheme.dark(
+          primary: Color(0xFF00ADB5),
         ),
-        initialRoute: '/',
-        routes: {
-          '/': (context) => const SplashScreen(),
-          '/members': (context) => const MembersScreen(),
-        },
       ),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const SplashScreen(),
+        '/members': (context) => const MembersScreen(),
+        '/detail': (context) => const MemberDetailScreen(),  // ← new
+      },
     );
   }
 }
