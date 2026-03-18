b3d98ff0bb13f4a9b17bd35f75712a5fa2d93fd7
jestoniandales25 <andalesjestoni1998@gmail.com>
2026-03-18 14:15:08 +0800

    feat: modified member model, remove unused import

--- lib/data/models/member_model.dart
+++ lib/data/models/member_model.dart
@@ -1,6 +1,5 @@
 // ignore_for_file: invalid_annotation_target
 import 'package:freezed_annotation/freezed_annotation.dart';
-import 'package:json_annotation/json_annotation.dart';
 
 part 'member_model.freezed.dart';
 part 'member_model.g.dart';
