a10299daaebe3b6b6411ee9bdc12d95596ad1e28
jestoniandales25 <andalesjestoni1998@gmail.com>
2026-03-19 10:40:44 +0800

    feat: modified changes on screens for navigation of bookmarks and video player

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:hololive_id_cards/blocs/bookmark_bloc_provider.dart';
 import 'package:hololive_id_cards/blocs/hololive_bloc_provider.dart';
+import 'package:hololive_id_cards/data/models/video_model.dart';
 import 'package:hololive_id_cards/data/repositories/hololive_repository.dart';
+import 'package:hololive_id_cards/ui/screens/bookmark_screen.dart';
 import 'package:hololive_id_cards/ui/screens/hololive_dashboard.dart';
 import 'package:hololive_id_cards/ui/screens/member_detail_screen.dart';
+import 'package:hololive_id_cards/ui/screens/video_player_screen.dart';
 import 'package:provider/provider.dart';
 
 void main() {
   runApp(
-    ChangeNotifierProvider(
-      create: (_) => HololiveBlocProvider(HololiveRepository())..loadMembers(),
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (_) =>
+              HololiveBlocProvider(HololiveRepository())..loadMembers(),
+        ),
+        ChangeNotifierProvider(
+          create: (_) => BookmarkBlocProvider()..loadBookmarks(),
+        ),
+      ],
       child: const MyApp(),
     ),
   );
@@ -30,6 +42,10 @@ class MyApp extends StatelessWidget {
       routes: {
         '/': (context) => const HololiveDashboard(),
         '/member-detail': (context) => const HololiveDetailScreen(),
+        '/video-player': (context) => VideoPlayerScreen(
+          video: ModalRoute.of(context)!.settings.arguments as VideoModel,
+        ),
+        '/bookmarks': (context) => const BookmarkScreen(),
       },
     );
   }
