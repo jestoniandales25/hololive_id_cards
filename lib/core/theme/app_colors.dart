52de8b0852d1871782985d39c928a332f02923a5
jestoniandales25 <andalesjestoni1998@gmail.com>
2026-03-30 16:20:17 +0800

    feat: added app_colors for centralized styles

---
+++ lib/core/theme/app_colors.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primary = Color(0xFF00ADB5);
+  static const Color primaryDark = Color(0xFF007A85);
+  static const Color background = Color(0xFF0D0D0D);
+  static const Color cardColor = Color(0xFF1A1A2E);
+  static const Color surface = Color(0xFF1A1A2E);
+  
+  static const Color textPrimary = Colors.white;
+  static const Color textSecondary = Colors.white54;
+  static const Color textMuted = Colors.white38;
+}
