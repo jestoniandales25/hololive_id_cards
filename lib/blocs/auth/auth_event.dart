74e5bb33485b88b4ecb56b20271d503f1c0a495c
jestoniandales25 <andalesjestoni1998@gmail.com>
2026-04-07 14:43:44 +0800

    feat: added auth bloc provider files from blocs folder

---
+++ lib/blocs/auth/auth_event.dart
@@ -0,0 +1,24 @@
+import 'package:equatable/equatable.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+abstract class AuthEvent extends Equatable {
+  const AuthEvent();
+
+  @override
+  List<Object?> get props => [];
+}
+
+class AuthUserChanged extends AuthEvent {
+  final User? user;
+
+  const AuthUserChanged(this.user);
+
+  @override
+  List<Object?> get props => [user];
+}
+
+class CheckAuthStatus extends AuthEvent {}
+
+class SignInWithGoogle extends AuthEvent {}
+
+class SignOut extends AuthEvent {}
