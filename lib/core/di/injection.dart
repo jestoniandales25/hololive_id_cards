3e1dca884748f878723a7856eb563bb19df9e592
jestoniandales25 <andalesjestoni1998@gmail.com>
2026-04-13 17:29:43 +0800

    feat: registered AutoRouter in the get_it dependency

--- lib/core/di/injection.dart
+++ lib/core/di/injection.dart
@@ -1,6 +1,7 @@
 import 'package:get_it/get_it.dart';
 import '../../data/repositories/auth_repository.dart';
 import '../../data/repositories/hololive_repository.dart';
+import '../navigation/app_router.dart';
 
 final getIt = GetIt.instance;
 
@@ -8,4 +9,7 @@ void setupDependencyInjection() {
   // Repositories
   getIt.registerLazySingleton<AuthRepository>(() => AuthRepository());
   getIt.registerLazySingleton<HololiveRepository>(() => HololiveRepository());
+
+  // Router
+  getIt.registerSingleton<AppRouter>(AppRouter());
 }
